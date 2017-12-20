15cd3ad0b8d059590a2d408201909183ab799800
zhi_da_di_yu@163.com <zhidadiyu@zhidadiyu-PC>
2017-12-20 19:33:45 +0800

    nihao,bilibili

---
+++ src/main/java/test/Test0.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:test
+ * File Name:Test0.java
+ * Package Name:test
+ * Date:2017年12月20日下午7:04:44
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package test;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午7:04:44 <br/>
+ * @author   zhidadiyu
+ * @version
+ * @see
+ */
+public class Test0 {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+
