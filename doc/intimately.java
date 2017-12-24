9d15665c7e7228353cd0e367aac65772a47baed0
zhidadiyu <zhi_da_di_yu@163.com>
2017-12-24 21:04:39 +0800

    First commit

---
+++ doc/intimately.java
@@ -0,0 +1,21 @@
+public class intimately{
+	int age;
+	String name;
+	int body;
+	int capture;
+	//这是无返回值
+	public void age(){
+		System.out.println("年龄为24");
+	}
+	public void name(){
+		System.out.println("郑河洋");
+	}
+	public void body(){
+		System.out.println("体重为90kg");
+	}
+	//这是有返回值
+	public int capture(){
+		return 1;
+	}
+
+}
