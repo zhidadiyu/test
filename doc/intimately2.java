1063e52201537279a5605a3f55c9b8c4980aa613
zhidadiyu <zhi_da_di_yu@163.com>
2017-12-24 21:54:09 +0800

    First commit

---
+++ doc/intimately2.java
@@ -0,0 +1,13 @@
+public class intimately2{
+	public static void main(String[] args) {
+		
+	intimately intimately;
+	intimately = new intimately();
+	intimately.age();
+	intimately.name();
+// System.out.println("猫的年龄："+intimately.age);
+// System.out.println("猫的体重："+intimately.name);
+	
+	System.out.println(intimately.capture());
+	}
+}
