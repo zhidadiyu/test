3364b14ec74da67604a20a89361dd93352797afd
zhidadiyu <zhi_da_di_yu@163.com>
2018-01-04 19:26:16 +0800

    2018_1_4

---
+++ src/main/java/test/2018_1_4/Test0.java
@@ -0,0 +1,11 @@
+  
+public class Test0 {
+	
+	public static void main(String[] args){
+		TestSubstring ts = new TestSubstring();
+		System.out.println(ts.getReplace());
+		TestSubstring ts0 = new TestSubstring();
+		System.out.println(ts0.getSubString());
+
+	}
+}
